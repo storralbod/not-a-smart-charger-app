b642adb842dc4d20509e0d04971f3f0e0fb9bead
Santi <storralbod@gmail.com>
2026-01-12 01:33:38 +0100

    adding suspense wrap in layout for dynamic rendering

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -16,15 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={"antialiased"}>
-        <div className="flex h-screen">
-          {/* Left Sidebar */}
-          <Sidebar />
+      <Suspense fallback={<div>Loading...</div>}>
+          <div className="flex h-screen">
+            {/* Left Sidebar */}
+            <Sidebar />
 
-          {/* Main Content */}
-          <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
-            {children}
-          </main>
-        </div>
+            {/* Main Content */}
+            <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+              {children}
+            </main>
+          </div>
+        </Suspense>
       </body>
     </html>
   );
